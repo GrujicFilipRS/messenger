d1f2c515d62213fe231e46125785519b85b3ed08
vector <vector@root.168.1.17>
2025-07-10 20:28:35 +0200

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Messenger.Models;
+
+namespace Messenger.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [Route("bubreg")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [Route("test")]
+    public IActionResult Test123()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
