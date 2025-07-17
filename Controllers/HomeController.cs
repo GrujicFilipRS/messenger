6aa3108161d96f52e864d457ca04d5c2bba2f5c6
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2025-07-17 16:41:21 +0200

    Added MessageModel into the database

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,8 +33,7 @@ public class HomeController : Controller
     [Route("messages")]
     public IActionResult Messages()
     {
-        int? name = HttpContext.Session.GetInt32("userid");
-        return Content($"Hello, {name}");
+        return View();
     }
 
     [HttpPost]
---
+++ Migrations/20250717143707_AddMessagesTable.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Messenger.Migrations
+{
+    public partial class AddMessagesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "username",
+                table: "Users",
+                newName: "Username");
+
+            migrationBuilder.RenameColumn(
+                name: "hashedPassword",
+                table: "Users",
+                newName: "HashedPassword");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Users",
+                newName: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    fromUserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    toUserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    messageText = table.Column<string>(type: "TEXT", nullable: false),
+                    messageSentTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Users",
+                newName: "username");
+
+            migrationBuilder.RenameColumn(
+                name: "HashedPassword",
+                table: "Users",
+                newName: "hashedPassword");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Users",
+                newName: "id");
+        }
+    }
+}
