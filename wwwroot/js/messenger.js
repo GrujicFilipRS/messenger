b54e8beca71b0f93e04b3e081e59acdb1cf68269
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2025-07-19 16:18:16 +0200

    Started logic of sending messages

--- wwwroot/js/messenger.js
+++ wwwroot/js/messenger.js
@@ -1,3 +1,20 @@
 const messageHeader = document.getElementById('message-header');
-
 messageHeader.style.display = 'none';
+
+// The following code will only be used to design the webpage
+// After the design has been made, it will be deleted
+
+let idToMessage = -1;
+
+function incrementUserIdToMessage() {
+    alert(idToMessage);
+    if (idToMessage == -1) {
+        idToMessage = 1;
+    }
+
+    idToMessage += 1;
+
+    if (idToMessage == currentUserId) {
+        idToMessage += 1;
+    }
+}
